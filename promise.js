5f343a9493f8d526499c6a29d1965151fccf2434
muhamadrizkymaulana11 <muhamadrizkymaulana11@gmail.com>
2022-09-16 21:10:34 +0700

    create function promise

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (expression) =>  {
+    const checkTheaterIXX = await promiseTheaterIXX(expression)
+    const checkTheaterVGC = await promiseTheaterVGC(expression)
+
+    
+    var countIXX = checkTheaterIXX.filter(function(element){
+      return element.hasil == expression;
+    }).length
+    
+    var countVGC = checkTheaterVGC.filter(function(element){
+      return element.hasil == expression;
+    }).length
+    
+    let count = countIXX + countVGC
+
+    return count 
+};
 
 module.exports = {
   promiseOutput,
